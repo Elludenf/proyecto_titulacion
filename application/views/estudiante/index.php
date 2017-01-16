6ec41bc1e087df90926e8e93d1ad524db99a19f2
RonnyCG <ronny1612695@gmail.com>
2017-01-16 09:23:33 -0500

    Se añadio las lineas de codigo dentro del metodo index() del controlador  de estudiante para la paginacion. En el modelo de estudiantes se agrego la funcion count para obtener el numero de filas totales y se añadieron lineas de codigo en el metodo get_all_estudiantes para la paginacion. En la view/index de estudiante se agrego los links para la paginacion. Tanto en el header como en el view de estudiantes se cambio la forma de referenciar los paths de los css, js e imagenes.

--- application/views/estudiante/index.php
+++ application/views/estudiante/index.php
@@ -1,14 +1,14 @@
 <div id ="nav-bar">
 
-    <div class = "logo2"><img src = "../assets/images/pantalla_main/logo_puce2.png"></div>
-    <div class = "icono-despliegue"><img src = "../assets/images/pantalla_main/icono_despliegue_lista.png"></div>
+    <div class = "logo2"><img src = "<?php echo base_url();?>assets/images/pantalla_main/logo_puce2.png"></div>
+    <div class = "icono-despliegue"><img src = "<?php echo base_url();?>assets/images/pantalla_main/icono_despliegue_lista.png"></div>
     <div class = "tipo-usuario">ADMINISTRADOR</div>
     <div class = "nombre-usuario">ADMINISTRADOR</div>
-    <div class = "icono-usuario"><img src = "../assets/images/pantalla_main/icono_usuario.png"></div>
+    <div class = "icono-usuario"><img src = "<?php echo base_url();?>assets/images/pantalla_main/icono_usuario.png"></div>
 
     <div class = "usuario-opciones">
         <div class = "usuario-opciones-desplegable">
-            <a href="" id="usuario-logout"> <img src="../assets/images/pantalla_main/icono_logout.png"> </a>
+            <a href="" id="usuario-logout"> <img src="<?php echo base_url();?>assets/images/pantalla_main/icono_logout.png"> </a>
             <div class="clear-desplegable"></div>
 
         </div>
@@ -39,7 +39,7 @@
                         </select>
                     </td>
                     <td>
-                        <input type="image" src="../assets/images/pantalla_main/icono_busqueda.png"  />
+                        <input type="image" src="<?php echo base_url();?>/assets/images/pantalla_main/icono_busqueda.png"  />
                     </td>
                 </tr>
             </table>
@@ -49,11 +49,11 @@
 
         <table border="0" width="100%" cellpadding="0" cellspacing="0" id="content-table">
             <tr>
-                <th rowspan="3" class="sized"><img src="../assets/images/shared/side_shadowleft.jpg" width="20" height="300" alt="" /></th>
+                <th rowspan="3" class="sized"><img src="<?php echo base_url();?>/assets/images/shared/side_shadowleft.jpg" width="20" height="300" alt="" /></th>
                 <th class="topleft"></th>
                 <td id="tbl-border-top">&nbsp;</td>
                 <th class="topright"></th>
-                <th rowspan="3" class="sized"><img src="../assets/images/shared/side_shadowright.jpg" width="20" height="300" alt="" /></th>
+                <th rowspan="3" class="sized"><img src="<?php echo base_url();?>/assets/images/shared/side_shadowright.jpg" width="20" height="300" alt="" /></th>
             </tr>
             <tr>
                 <td id="tbl-border-left"></td>
@@ -64,14 +64,11 @@
                         <!--  start table-content  -->
                         <div id="table-content">
 
-
-
-
                             <!--  start product-table ..................................................................................... -->
                             <form id="mainform" action="">
                                 <table border="0" width="100%" cellpadding="0" cellspacing="0" id="product-table">
                                     <tr>
-                                        <th class="table-header-check"><a id="toggle-all" ></a> </th>
+
                                         <th class="table-header-repeat line-left minwidth-1"><a href="">Codigo</a>	</th>
                                         <th class="table-header-repeat line-left minwidth-1"><a href="">Rol</a>	</th>
                                         <th class="table-header-repeat line-left minwidth-1"><a href="">1er Nombre</a></th>
@@ -100,7 +97,7 @@
                                     </tr>
                                     <?php foreach($estudiante as $e){ ?>
                                         <tr>
-                                            <td></td>
+
                                             <td><?php echo $e['per_codigo']; ?></td>
                                             <td><?php echo $e['rol_codigo']; ?></td>
                                             <td><?php echo $e['per_nombre1']; ?></td>
@@ -128,47 +125,15 @@
 
                                         </tr>
                                     <?php } ?>
-
                                 </table>
+
+                                <?php foreach ($links as $link) {
+                                    echo "<li>". $link."</li>";
+                                } ?>
                                 <!--  end product-table................................... -->
                             </form>
                         </div>
                         <!--  end content-table  -->
-
-                        <!--  start actions-box ............................................... -->
-                        <div id="actions-box">
-                            <a href="" class="action-slider"></a>
-                            <div id="actions-box-slider">
-                                <a href="<?php echo site_url(); ?>" class="action-edit">Editar</a>
-                                <a href="<?php echo site_url(); ?>" class="action-delete">Eliminar</a>
-                            </div>
-                            <div class="clear"></div>
-                        </div>
-                        <!-- end actions-box........... -->
-
-                        <!--  start paging..................................................... -->
-                        <table border="0" cellpadding="0" cellspacing="0" id="paging-table">
-                            <tr>
-                                <td>
-                                    <a href="" class="page-far-left"></a>
-                                    <a href="" class="page-left"></a>
-                                    <div id="page-info">Pagina <strong>1</strong> / 15</div>
-                                    <a href="" class="page-right"></a>
-                                    <a href="" class="page-far-right"></a>
-                                </td>
-                                <td>
-                                    <select  class="styledselect_pages">
-                                        <option value="">Numero de filas</option>
-                                        <option value="">1</option>
-                                        <option value="">2</option>
-                                        <option value="">3</option>
-                                    </select>
-                                </td>
-                            </tr>
-                        </table>
-                        <!--  end paging................ -->
-
-
                     </div>
                     <!--  end content-table-inner ............................................END  -->
                 </td>
