dbc1586ef87426bb9b5c13d96f0fd79366bf6e55
robcga1 <robcga1@gmail.com>
2017-01-18 10:20:08 -0500

    correccion del Login_Model

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -1,15 +1,11 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
- 
-class Login_model extends CI_Model
+Class Login_model extends CI_Model
 {
- 
-
     public function login($username,$password) {
-        $this -> db -> select('zusrcodigo, zusrnombre, zusrclave');
-        $this -> db -> from('zusuarios');
-        $this -> db -> where('zusrnombre', $username);
-        $this -> db -> where('zusrclave', $password);
+        $this -> db -> select('per_codigo, per_mailpuce, per_clave');
+        $this -> db -> from('estudiante');
+        $this -> db -> where('per_mailpuce', $username);
+        $this -> db -> where('per_clave', $password);
         $this -> db -> limit(1);
 
         $query = $this -> db -> get();
@@ -20,11 +16,23 @@ class Login_model extends CI_Model
         }
         else
         {
-            return false;
+            $this -> db -> select('per_codigo, per_mailpuce, per_clave');
+            $this -> db -> from('profesor');
+            $this -> db -> where('per_mailpuce', $username);
+            $this -> db -> where('per_clave', $password);
+            $this -> db -> limit(1);
+
+            $query = $this -> db -> get();
+
+            if($query -> num_rows() == 1)
+            {
+                return $query->result();
+            }else{
+                return false;
+            }
+
         }
     }
- 
-    function __destruct() {
-        $this->db->close();
-    }
+
 }
+?>
