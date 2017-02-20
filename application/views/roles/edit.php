0d9a3dc5aff474724a32f28b7122610b2e30c0c0
henryt028 <daniel.zero273@hotmail.com>
2017-02-20 11:12:49 -0500

    Update

--- application/views/roles/edit.php
+++
@@ -1,9 +0,0 @@
-<?php echo validation_errors(); ?>
-
-<?php echo form_open('rol/edit/'.$rol['rol_codigo']); ?>
-
-	<div>Rol Descripcion : <input type="text" name="rol_descripcion" value="<?php echo ($this->input->post('rol_descripcion') ? $this->input->post('rol_descripcion') : $rol['rol_descripcion']); ?>" /></div>
-	
-	<button type="submit">Save</button>
-	
-<?php echo form_close(); ?>
