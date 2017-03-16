96781b6f052ae5de5972d4b52e26417817192ba0
Elludenf <pperezvallejo@gmail.com>
2017-03-16 18:44:30 -0500

    UPDATED: Controller -Carrera restrict path HTTP_REFER

--- application/controllers/Carrera.php
+++ application/controllers/Carrera.php
@@ -10,6 +10,7 @@ class Carrera extends CI_Controller
     {
         parent::__construct();
         $this->load->model('Carrera_model');
+        $this->load->helper('url');
 
     }
 
@@ -17,144 +18,165 @@ class Carrera extends CI_Controller
      * Listing of carreras
      */
     private $limit = 5;
+
     function index()
     {
-        $data['carreras'] = $this->Carrera_model->get_all_carreras();
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            $data['carreras'] = $this->Carrera_model->get_all_carreras();
 
-        /*Empiezo de paginacion*/
-        $total_rows = $this->Carrera_model->count();
+            /*Empiezo de paginacion*/
+            $total_rows = $this->Carrera_model->count();
 
-        $this->load->library('pagination');
-        $config['total_rows'] = $total_rows;
-        $config['per_page'] = $this->limit;
-        $config['uri_segment'] = 3;
-        $config['base_url'] = base_url().'/carrera/index';
+            $this->load->library('pagination');
+            $config['total_rows'] = $total_rows;
+            $config['per_page'] = $this->limit;
+            $config['uri_segment'] = 3;
+            $config['base_url'] = base_url() . '/carrera/index';
 
-        //CSS
-        $config['full_tag_open'] = '<ul class="tsc_pagination tsc_paginationA tsc_paginationA01">';
-        $config['full_tag_close'] = '</ul>';
-        $config['prev_link'] = '&lt;';
-        $config['prev_tag_open'] = '<li>';
-        $config['prev_tag_close'] = '</li>';
-        $config['next_link'] = '&gt;';
-        $config['next_tag_open'] = '<li>';
-        $config['next_tag_close'] = '</li>';
-        $config['cur_tag_open'] = '<li class="current"><a href="#">';
-        $config['cur_tag_close'] = '</a></li>';
-        $config['num_tag_open'] = '<li>';
-        $config['num_tag_close'] = '</li>';
+            //CSS
+            $config['full_tag_open'] = '<ul class="tsc_pagination tsc_paginationA tsc_paginationA01">';
+            $config['full_tag_close'] = '</ul>';
+            $config['prev_link'] = '&lt;';
+            $config['prev_tag_open'] = '<li>';
+            $config['prev_tag_close'] = '</li>';
+            $config['next_link'] = '&gt;';
+            $config['next_tag_open'] = '<li>';
+            $config['next_tag_close'] = '</li>';
+            $config['cur_tag_open'] = '<li class="current"><a href="#">';
+            $config['cur_tag_close'] = '</a></li>';
+            $config['num_tag_open'] = '<li>';
+            $config['num_tag_close'] = '</li>';
 
-        $config['first_tag_open'] = '<li>';
-        $config['first_tag_close'] = '</li>';
-        $config['last_tag_open'] = '<li>';
-        $config['last_tag_close'] = '</li>';
+            $config['first_tag_open'] = '<li>';
+            $config['first_tag_close'] = '</li>';
+            $config['last_tag_open'] = '<li>';
+            $config['last_tag_close'] = '</li>';
 
-        $config['first_link'] = '&lt;&lt;';
-        $config['last_link'] = '&gt;&gt;';
-        //Fin CSS
-        $this->pagination->initialize($config);
+            $config['first_link'] = '&lt;&lt;';
+            $config['last_link'] = '&gt;&gt;';
+            //Fin CSS
+            $this->pagination->initialize($config);
 
-        $page_links = $this->pagination->create_links();
-        $data['links'] = explode('&nbsp;',$page_links );
-        /*Fin de paginacion*/
+            $page_links = $this->pagination->create_links();
+            $data['links'] = explode('&nbsp;', $page_links);
+            /*Fin de paginacion*/
 
-        $this->load->helper('form');
-        $this->load->helper(array('form'));
-        $this->load->view('templates/header');
-        $this->load->view('carreras/index', $data);
-        $this->load->view('templates/footer');
+            $this->load->helper('form');
+            $this->load->helper(array('form'));
+            $this->load->view('templates/header');
+            $this->load->view('carreras/index', $data);
+            $this->load->view('templates/footer');
+
+        } else {
+            $this->load->view('templates/header');
+            $this->load->view('templates/forbidden');
+            $this->load->view('templates/footer');
+        }
     }
 
     /*
      * Adding a new carrera
      */
     function add()
-    {   
-        $this->load->library('form_validation');
+    {
+        if (isset($_SERVER['HTTP_REFERER'])) {
 
-		$this->form_validation->set_rules('esc_codigo','Esc Codigo','required|integer');
-		$this->form_validation->set_rules('carr_descripcion','Carr Descripcion','required|max_length[50]');
-		
-		if($this->form_validation->run())     
-        {
-            //$this->db->select_max('carr_codigo');
-            //$result= $this->db->get('carreras')->row_array();
-            $params = array(
-                //'carr_codigo' =>$result['carr_codigo']+1,
-				'esc_codigo' => $this->input->post('esc_codigo'),
-				'carr_descripcion' => $this->input->post('carr_descripcion'),
-            );
-            
-            $carrera_id = $this->Carrera_model->add_carrera($params);
-            redirect('carrera/index');
-        }
-        else
-        {
+            $this->load->library('form_validation');
 
-			$this->load->model('Escuela_model');
-			$data['all_escuelas'] = $this->Escuela_model->get_all_escuelas_();
+            $this->form_validation->set_rules('esc_codigo', 'Esc Codigo', 'required|integer');
+            $this->form_validation->set_rules('carr_descripcion', 'Carr Descripcion', 'required|max_length[50]');
+
+            if ($this->form_validation->run()) {
+                //$this->db->select_max('carr_codigo');
+                //$result= $this->db->get('carreras')->row_array();
+                $params = array(
+                    //'carr_codigo' =>$result['carr_codigo']+1,
+                    'esc_codigo' => $this->input->post('esc_codigo'),
+                    'carr_descripcion' => $this->input->post('carr_descripcion'),
+                );
+
+                $carrera_id = $this->Carrera_model->add_carrera($params);
+                redirect('carrera/index');
+            } else {
+
+                $this->load->model('Escuela_model');
+                $data['all_escuelas'] = $this->Escuela_model->get_all_escuelas_();
+                $this->load->view('templates/header');
+                $this->load->view('carreras/add', $data);
+                $this->load->view('templates/footer');
+            }
+        } else {
+
             $this->load->view('templates/header');
-            $this->load->view('carreras/add',$data);
+            $this->load->view('templates/forbidden');
             $this->load->view('templates/footer');
+
         }
-    }  
+    }
 
     /*
      * Editing a carrera
      */
     function edit($carr_codigo)
-    {   
-        // check if the carrera exists before trying to edit it
-        $carrera = $this->Carrera_model->get_carrera($carr_codigo);
-        
-        if(isset($carrera['carr_codigo']))
-        {
-            $this->load->library('form_validation');
+    {
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            // check if the carrera exists before trying to edit it
+            $carrera = $this->Carrera_model->get_carrera($carr_codigo);
 
-			$this->form_validation->set_rules('esc_codigo','Esc Codigo','required|integer');
-			$this->form_validation->set_rules('carr_descripcion','Carr Descripcion','required|max_length[50]');
-		
-			if($this->form_validation->run())     
-            {   
-                $params = array(
-					'esc_codigo' => $this->input->post('esc_codigo'),
-					'carr_descripcion' => $this->input->post('carr_descripcion'),
-                );
+            if (isset($carrera['carr_codigo'])) {
+                $this->load->library('form_validation');
 
-                $this->Carrera_model->update_carrera($carr_codigo,$params);            
-                redirect('carrera/index');
-            }
-            else
-            {   
-                $data['carrera'] = $this->Carrera_model->get_carrera($carr_codigo);
-    
-				$this->load->model('Escuela_model');
-				$data['all_escuelas'] = $this->Escuela_model->get_all_escuelas_();
+                $this->form_validation->set_rules('esc_codigo', 'Esc Codigo', 'required|integer');
+                $this->form_validation->set_rules('carr_descripcion', 'Carr Descripcion', 'required|max_length[50]');
 
-                $this->load->view('templates/header');
-                $this->load->view('carreras/edit',$data);
-                $this->load->view('templates/footer');
-            }
+                if ($this->form_validation->run()) {
+                    $params = array(
+                        'esc_codigo' => $this->input->post('esc_codigo'),
+                        'carr_descripcion' => $this->input->post('carr_descripcion'),
+                    );
+
+                    $this->Carrera_model->update_carrera($carr_codigo, $params);
+                    redirect('carrera/index');
+                } else {
+                    $data['carrera'] = $this->Carrera_model->get_carrera($carr_codigo);
+
+                    $this->load->model('Escuela_model');
+                    $data['all_escuelas'] = $this->Escuela_model->get_all_escuelas_();
+
+                    $this->load->view('templates/header');
+                    $this->load->view('carreras/edit', $data);
+                    $this->load->view('templates/footer');
+                }
+            } else
+                show_error('The carrera you are trying to edit does not exist.');
+        } else {
+
+
+            $this->load->view('templates/header');
+            $this->load->view('templates/forbidden');
+            $this->load->view('templates/footer');
+
         }
-        else
-            show_error('The carrera you are trying to edit does not exist.');
-    } 
+    }
 
     /*
      * Deleting carrera
      */
     function remove($carr_codigo)
     {
-        $carrera = $this->Carrera_model->get_carrera($carr_codigo);
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            $carrera = $this->Carrera_model->get_carrera($carr_codigo);
 
-        // check if the carrera exists before trying to delete it
-        if(isset($carrera['carr_codigo']))
-        {
-            $this->Carrera_model->delete_carrera($carr_codigo);
-            redirect('carrera/index');
+            // check if the carrera exists before trying to delete it
+            if (isset($carrera['carr_codigo'])) {
+                $this->Carrera_model->delete_carrera($carr_codigo);
+                redirect('carrera/index');
+            } else
+                show_error('The carrera you are trying to delete does not exist.');
+        } else {
+            $this->load->view('templates/header');
+            $this->load->view('templates/forbidden');
+            $this->load->view('templates/footer');
         }
-        else
-            show_error('The carrera you are trying to delete does not exist.');
     }
-    
 }
