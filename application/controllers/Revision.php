f51919770694bdb8319d539e5af0e99d8d0cb93a
chaloxavi <chaloxavi@hotmail.com>
2017-03-22 23:30:43 -0500

    -se corregieron errores

--- application/controllers/Revision.php
+++ application/controllers/Revision.php
@@ -97,7 +97,12 @@ class Revision extends CI_Controller
                 );
 
                 $revision_id = $this->Revision_model->add_revision($params);
-                redirect('revision/index');
+                if ($this->session->__get('rol_group') == 'R_PROFESOR') {
+                    redirect('profesor/perfil');
+                } else {
+                    redirect('revision/index');
+                }
+
             } else {
 
                 $this->load->model('Trabajo_disertacion_model');
@@ -113,6 +118,7 @@ class Revision extends CI_Controller
                     $prof = $this->Profesor_model->get_profesor($revdir['prof_codigo']);
                     $a_prof[$c] = $prof;
                     $c++;
+                break;
                 }
 
                 $this->load->model('Profesor_model');
